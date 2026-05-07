ea98d7f6a1ed0c8d1324b8a38a8958d973e419c9
1378484330@qq.com <1378484330@qq.com>
2026-05-07 20:08:53 +0800

    Fine tune generation entry scroll

--- public/app.js
+++ public/app.js
@@ -598,7 +598,7 @@ function shouldShowHero() {
   return state.forceHero || !state.showGenerationView;
 }
 
-function showHomeHero({ focusPrompt = false, smooth = false, scrollTop = 120 } = {}) {
+function showHomeHero({ focusPrompt = false, smooth = false, scrollTop = 130 } = {}) {
   state.view = "home";
   state.forceHero = true;
   state.showGenerationView = false;
