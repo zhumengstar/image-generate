6defee9145f4b183e55302de867204fdc78b1f4a
1378484330@qq.com <1378484330@qq.com>
2026-05-07 20:16:03 +0800

    Nudge generation entry scroll

--- public/app.js
+++ public/app.js
@@ -598,7 +598,7 @@ function shouldShowHero() {
   return state.forceHero || !state.showGenerationView;
 }
 
-function showHomeHero({ focusPrompt = false, smooth = false, scrollTop = 150 } = {}) {
+function showHomeHero({ focusPrompt = false, smooth = false, scrollTop = 160 } = {}) {
   state.view = "home";
   state.forceHero = true;
   state.showGenerationView = false;
